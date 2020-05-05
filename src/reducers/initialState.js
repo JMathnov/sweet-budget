87f6755d42f49d859f6b76a7048ea622324d5dce
jmathnov <jason.mathnov@joinhoney.com>
2020-05-05 13:35:12 -0600

    cleanup

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -14,5 +14,27 @@ export default {
       annual: 0,
       threeYear: 0
     }
-  }
+  },
+
+  essentialItems: {
+    toilet_paper: [{id: 'uuid', blacklisted: null}],
+    gloves: [],
+    mask: [],
+    soap: [],
+    toothpaste: [],
+    sanitizer_wipes: [],
+    tissues: [],
+    napkins: [],
+    paper_towels: []
+  },
+
+  shopping_list: {
+    items: [
+      {name: 'toilet_paper', quantity: 5, limit_price:  null},
+      {name: 'gloves', quantity: 2, limit_price:  null},
+      {name: 'soap', quantity: 1, limit_price:  null}
+    ]
+  },
+
+
 };
