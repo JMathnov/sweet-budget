41e57ccc0d402ab568277efe6d859489b92e98f2
jmathnov <jason.mathnov@joinhoney.com>
2020-05-06 09:54:45 -0600

    Added a bunch of sample data pulled from BQ for each essential item type

--- src/components/pages/CurateShoppingList/ShoppingItemRow.js
+++ src/components/pages/CurateShoppingList/ShoppingItemRow.js
@@ -5,6 +5,8 @@ import {bindActionCreators} from 'redux';
 import * as actions from '../../../actions/sweetBudgetActions';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import _ from 'lodash';
+import {median} from 'mathjs';
 
 import Slider from '@material-ui/core/Slider';
 
@@ -13,16 +15,36 @@ export class ShoppingItemRow extends React.Component {
   propTypes = {
     actions: PropTypes.object.isRequired,
     item: PropTypes.object.isRequired,
-    essentialItems: PropTypes.arrayOf(PropTypes.object).isRequired
+    essentialItems: PropTypes.object
   };
 
   getProductAveragePrice(item) {
+    const items = _.get(this.props.essentialItems, item.name, {});
+
+    const allowedItems = _.filter(items, item=> !item.blacklisted, []);
+
+    if(allowedItems.length == 0){
+      return {};
+    } else {
+      const prices = allowedItems.map(item=> item.price).sort();
+      const cheapest = prices[0];
+      const mostExpensive = prices[prices.length - 1];
+
+      return {
+        cheapest,
+        mostExpensive,
+        median: median(prices)
+      };
+    }
+
+
 
   }
 
   render() {
 
-
+    const priceProfile = this.getProductAveragePrice(this.props.item);
+    debugger;
     return (
       <Grid container spacing={1}>
           <Grid item xs={2}>
@@ -48,7 +70,8 @@ export class ShoppingItemRow extends React.Component {
 function mapStateToProps(state) {
   console.log(state);
   return {
-    shopping_list: state.shoppingList.shopping_list
+    shopping_list: state.shoppingList.shopping_list,
+    essentialItems: state.shoppingList.essentialItems,
   };
 }
 
