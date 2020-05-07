a692a4082b698fb18be2544eb1d52f9ee64ca9cf
jmathnov <jason.mathnov@joinhoney.com>
2020-05-07 11:24:10 -0600

    format & style

--- src/components/pages/NewShoppingList/NewShoppingList.js
+++ src/components/pages/NewShoppingList/NewShoppingList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 import withStyles from "@material-ui/styles/withStyles";
 import Button from "@material-ui/core/Button";
@@ -54,17 +54,17 @@ const styles = theme => ({
 const ShoppingItemRow = ({item, adjust}) => (
   <div>
     <Button
-    variant="outlined"
-    color="primary"
-    onClick={() => adjust("+", item)}>+</Button>
+      variant="outlined"
+      color="primary"
+      onClick={() => adjust("+", item)}>+</Button>
     <Button
-    variant="outlined"
-    color="primary"
-    onClick={() => adjust("-", item)}>-</Button>
+      variant="outlined"
+      color="primary"
+      onClick={() => adjust("-", item)}>-</Button>
     <div>x{item.quantity}</div>
     <div>{item.name}</div>
   </div>
-)
+);
 
 export class NewShoppingList extends React.Component {
   constructor(props) {
@@ -74,43 +74,43 @@ export class NewShoppingList extends React.Component {
 
   adjustQauntity = (sign, item) => {
     console.log("Adjust Quantity")
-  }
+  };
 
   addItemToCart = (item) => {
     const shoppingListItem = {
       ...item,
       quantity: 1,
-      limit_price:  null,
+      limit_price: null,
       goodUntil: null,
       purchasePrice: null,
       honeyGold: null
     };
 
     this.props.actions.addProductToSoppingList(shoppingListItem);
-  }
+  };
 
   render() {
-    const { classes } = this.props;
+    const {classes} = this.props;
 
     return (
       <div className={classes.root}>
         <Grid container spacing={0} justify="center" className={classes.grid}>
           <Grid item md={9}>
-            <Paper className={classes.paper} style={{ position: "relative" }}>
-                <Typography variant="h5" gutterBottom>
-                  Shop Essential Items
-                </Typography>
-                <Typography variant="body2">
-                  Select product categories for your budget
-                </Typography>
-                <CategoryList items={this.props.product_category_list} onClick={this.addItemToCart} />
+            <Paper className={classes.paper} style={{position: "relative"}}>
+              <Typography variant="h5" gutterBottom>
+                Shop Essential Items
+              </Typography>
+              <Typography variant="body2">
+                Select product categories for your budget
+              </Typography>
+              <CategoryList items={this.props.product_category_list} onClick={this.addItemToCart}/>
             </Paper>
           </Grid>
           <Grid item xs={12} md={3} className={classes.grid3}>
-            <Paper className={classes.paper} style={{ position: "relative" }}>
+            <Paper className={classes.paper} style={{position: "relative"}}>
               <Grid container justify="center" className={classes.rightPanel}>
                 <Typography variant="h5" gutterBottom>
-                Shopping List
+                  Shopping List
                 </Typography>
                 <Grid container justify="center">
                   {this.props.shopping_list.items.map((item) =>
@@ -122,7 +122,7 @@ export class NewShoppingList extends React.Component {
                 <Grid item>
                   <div className={classes.buttonBar}>
                     <Button
-                      to={{ pathname: "/curate-shopping-list" }}
+                      to={{pathname: "/curate-shopping-list"}}
                       component={Link}
                       variant="outlined"
                       color="primary"
@@ -134,8 +134,8 @@ export class NewShoppingList extends React.Component {
               </Grid>
             </Paper>
           </Grid>
-      </Grid>
-    </div>
+        </Grid>
+      </div>
     );
   }
 }
